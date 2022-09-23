42ae2254201a02d264837ad58319e5a8d1967f62
FeiselxG <faiselrehima2@gmail.com>
2022-09-23 08:23:45 -0700

    0x01

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,28 +14,28 @@ int main(void)
 		for (i = 48; i <= 57; i++)
 		{
 			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
 				{
-					for (j = 48; j <= 57; j++)
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
 					{
-						if (((k + j) > (c + i) &&  k >= c) || c < k)
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+						if (c + i + k + j == 227 && c == 57)
 						{
-							putchar(c);
-							putchar(i);
+							break;
+						}
+						else
+						{
+							putchar(',');
 							putchar(' ');
-							putchar(k);
-							putchar(j);
-							if (c + i + k + j == 227 && c == 57)
-							{
-								break;
-							}
-							else
-							{
-								putchar(',');
-								putchar(' ');
-							}
 						}
 					}
 				}
+			}
 		}
 	}
 
