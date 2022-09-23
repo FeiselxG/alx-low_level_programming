3dd51b50506b2a8c39f60a480ce8e01a2fa394af
FeiselxG <faiselrehima2@gmail.com>
2022-09-23 06:56:22 -0700

    strrev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "strhead.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: An array of integers
+ * @n: Number of elements to swap
+ * 
+ * Return: empty
+ */
+
+void reverse_array(int *a, int n)
+{	
+
+	int i,*r,j;
+
+    	i = 0;
+	j = n-1;
+
+        while (i < n)
+	{
+		p[i] = a[j]
+		i++;
+		j--;
+        }
+
+	a = r;
+
+}
